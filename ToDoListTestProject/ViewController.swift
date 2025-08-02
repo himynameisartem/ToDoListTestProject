1f553577315dfd734296bcaf04ef4e0b7c39552c
Artem Kudryavtsev <himynameisartem@icloud.com>
2025-08-02 11:25:40 +0300

    Initial Commit

---
+++ ToDoListTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListTestProject
+//
+//  Created by Artem Kudryavtsev on 02.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
